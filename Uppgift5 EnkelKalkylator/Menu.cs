836c7e03b7634e318bd46fb20c64715ddf3e3f91
AiWictoria <Viktoria.pakhrova@outlook.com>
2024-09-27 13:39:07 +0200

    Skapat en meny med de originala operationer

--- Uppgift5 EnkelKalkylator/Menu.cs
+++ Uppgift5 EnkelKalkylator/Menu.cs
@@ -9,7 +9,13 @@ namespace EnkelKalkylator
 
         internal void MainMenu()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(" ---- Miniräknare ---- \n" +
+                "\n\tVälj en av följande operationer för vad du önskar att göra:" +
+                "\n\t1. Addera" +
+                "\n\t2. Subtrahera" +
+                "\n\t3. Dividera" +
+                "\n\t4. Multiplicera" +
+                "\n\t0. Avsluta programmet");
         }
     }
 }
