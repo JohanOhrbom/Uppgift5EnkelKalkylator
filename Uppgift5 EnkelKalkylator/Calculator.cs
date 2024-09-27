d8560c42b38697c0a5f3f51bc19e74bdc8c294a6
Johan <Johan.0hrbom@gmail.com>
2024-09-27 14:06:08 +0200

    Lagt till medlemmar Tal1 och Tal2 till class Calculator.

--- Uppgift5 EnkelKalkylator/Calculator.cs
+++ Uppgift5 EnkelKalkylator/Calculator.cs
@@ -2,17 +2,24 @@
 namespace EnkelKalkylator
 {
     internal class Calculator
-    {
+    {//Internal class Calc
+        public double Tal1 {  get; set; }
+        public double Tal2 { get; set; }
+
+
+
         public Menu menu = new Menu();
         public Calculator()
         {
-
+            
 
         }
+
+
 
         internal void Run()
         {
             menu.MainMenu();
         }
-    }
+    }//Internal class Calc end
 }
