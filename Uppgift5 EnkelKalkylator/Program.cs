3bf892228a72e28125a564ab5468e0323b79c7db
AiWictoria <Viktoria.pakhrova@outlook.com>
2024-09-30 14:15:26 +0200

    Tagit bort urkommenterad originalprogram och lagt  till en kommentar på Program.cs

--- Uppgift5 EnkelKalkylator/Program.cs
+++ Uppgift5 EnkelKalkylator/Program.cs
@@ -5,39 +5,8 @@
 
         static void Main(string[] args)
         {
-
-            //Console.WriteLine("Enkel Kalkylator");
-            //Console.WriteLine("Ange operation (+, -, *, /): ");
-            //char operation = Console.ReadKey().KeyChar;
-            //Console.WriteLine("\nAnge första talet: ");
-            //double num1 = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine("Ange andra talet: ");
-            //double num2 = Convert.ToDouble(Console.ReadLine());
-
-            //double result = 0;
-            //switch (operation)
-            //{
-            //    case '+':
-            //        result = num1 + num2;
-            //        break;
-            //    case '-':
-            //        result = num1 - num2;
-            //        break;
-            //    case '*':
-            //        result = num1 * num2;
-            //        break;
-            //    case '/':
-            //        result = num1 / num2;
-            //        break;
-            //    default:
-            //        Console.WriteLine("Ogiltig operation.");
-            //        return;
-            //}
-
-            //Console.WriteLine($"Resultat: {result}");
-            //Console.ReadKey();
-            
             var menu = new Menu();
+            //Anropar metod för meny från klass menu
             menu.MainMenu();
         }
     }
